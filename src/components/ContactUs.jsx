5e472b76a8b0fcd329d69dda7cd3b1200d4d1170
medievalshell <medievalshell@gmail.com>
2025-10-22 15:22:40 +0200

    Update Added X and Mastodon and removed Phone and Address

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -128,20 +128,6 @@ const ContactUs = forwardRef(({
                                     <span className="font-medium text-white mr-2">Email:</span>
                                     armsx2mail@gmail.com
                                 </li>
-                                <li className="flex items-center">
-                                    <Phone className={`w-5 h-5 mr-4 ${PRIMARY_COLOR} flex-shrink-0`} />
-                                    <span className="font-medium text-white mr-2">Telephone:</span>
-                                    +66 04032000
-                                </li>
-                                <li className="flex items-start">
-                                    <MapPin className={`w-5 h-5 mr-4 ${PRIMARY_COLOR} flex-shrink-0 mt-1`} />
-                                    <div>
-                                        <span className="font-medium text-white block">Address:</span>
-                                        Hacker Road, 1
-                                        <br />
-                                        Silicon Valley, Cupertino
-                                    </div>
-                                </li>
                             </ul>
                         </div>
                         
